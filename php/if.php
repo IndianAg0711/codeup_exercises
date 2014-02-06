cc086941143033eae9c1253f91d70d02840601cc
IndianAg0711 <joseph.villafranca@gmail.com>
2014-02-06 14:51:33 -0600

    if exercise v2

--- php/if.php
+++ php/if.php
@@ -1,39 +1,30 @@
 <?php
 
-$a = 5;
+$a = 15;
 $b = 10;
-$c = '10';
+$c = 10;
 
 if ($a < $b) {
 	echo "$a is less than $b\n";
-}
-
-if ($b > $a) {
-	echo "$b is greater than $a\n";
+}	else {
+		echo "$a is greater than $b\n";
 }
 
 if ($b >= $c) {
 	echo "$b is greater than or equal to $c\n";
+}	else {
+		echo "$b is less than $c\n";
 }
 
-if ($b <= $c) {
-	echo "$b is less than or equal to $c\n";
-}
-
-if ($b == $c) {
-	echo "$b is equal to $c\n";
-}
 
 if ($b === $c) {
 	echo "$b is identical to $c\n";
-}
-
-if ($b != $c) {
-	echo "$b is not equal to $c\n";
-}
-
-if ($b !== $c) {
-	echo "$b is not identical to $c\n";
-}
+}	elseif ($b == $c) {
+		echo "$b is equal to $c\n";
+	}	elseif (($b !== $c) && ($b == $c)) {
+			echo "$b is not identical to $c\n";
+		}	else {
+				echo "$b is not equal to $c\n";
+			}
 
 ?>
