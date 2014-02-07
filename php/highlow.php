65fe780bea3b69a1a281b7797e6705767995ebc2
IndianAg0711 <joseph.villafranca@gmail.com>
2014-02-07 11:14:51 -0600

    # of guesses

--- php/highlow.php
+++ php/highlow.php
@@ -12,6 +12,10 @@ fwrite(STDOUT, "Guess a number between 1 and 100: ");
 
 $user_guess = fgets(STDIN);
 
+$numGuess = 0;
+
+$numGuess++;
+
 while ($user_guess != $game_guess) {
 	if ($user_guess < $game_guess) {
 		echo "Higher!\n";
@@ -21,8 +25,19 @@ while ($user_guess != $game_guess) {
 		}
 	echo "Guess again: "	;
 	$user_guess = fgets(STDIN);
+	$numGuess++;
 }
+
 echo "Congrats {$first_name} you've guessed my number!\n";
+
+echo "It took you $numGuess guesses\n";
+
+	if ($numGuess <= 5) {
+		echo "You're an effin champ!\n";
+	}
+		else {
+			echo "You're average.\n";
+		}
 
 exit(0);
 
