5f306fe7458b9a7e66f3b65cd9ce3cac41b41b7f
IndianAg0711 <joseph.villafranca@gmail.com>
2014-02-07 16:00:00 -0600

    with count by prompt

--- php/for.php
+++ php/for.php
@@ -8,11 +8,17 @@ echo "Please enter a number to stop counting at: ";
 
 $end = fgets(STDIN);
 
+echo "Please enter a number to count by: ";
+
+$countBy = fgets(STDIN);
+
+$countBy = intval($countBy);
+
 $begin = intval($begin);
 
 $end = intval($end);
 
-for ($a = $begin; $a <= $end; $a++) {
+for ($a = $begin; $a <= $end; $a += $countBy) {
 	echo "$a\n";
 }
 
