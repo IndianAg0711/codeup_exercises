19a3853db9e874a56449b6f461039ab42cbb3978
IndianAg0711 <joseph.villafranca@gmail.com>
2014-02-07 16:21:55 -0600

    error messages

--- php/for.php
+++ php/for.php
@@ -4,22 +4,32 @@ echo "Please enter a number to start counting from: ";
 
 $begin = fgets(STDIN);
 
+$begin = intval($begin);
+
+if ($begin == 0) {
+	echo "Error, please enter a number.\n";
+	exit(0);
+}
+
 echo "Please enter a number to stop counting at: ";
 
 $end = fgets(STDIN);
+
+$end = intval($end);
+
+if ($end == 0) {
+	echo "Error, please enter a number.\n";
+	exit(0);
+}
 
 echo "Please enter a number to count by: ";
 
 $countBy = fgets(STDIN);
 
-$begin = intval($begin);
+$countBy = intval($countBy);
 
-$end = intval($end);
-
-if (!is_numeric($countBy)) {
+if ($countBy == 0) {
 	$countBy = 1;
-}	else {
-	$countBy = intval($countBy);
 }
 
 for ($a = $begin; $a <= $end; $a += $countBy) {
