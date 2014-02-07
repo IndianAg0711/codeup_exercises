b76c7644ecfb21c91ea736b344285404444409a7
IndianAg0711 <joseph.villafranca@gmail.com>
2014-02-07 15:14:36 -0600

    High/Low with arguments

--- php/highlow.php
+++ php/highlow.php
@@ -1,14 +1,25 @@
 <?php
 
+$min = $argv[1];
+$max = $argv[2];
+
+
+if (($argc == 3) && ($argv[1] < $argv[2]) && (is_numeric($argv[1])) && (is_numeric($argv[2]))) {
+} else {
+	echo "Error: User did not set min/max. Defaults used: 1 and 100\n";
+	$min = 1;
+	$max = 100;
+}
+
 fwrite(STDOUT, "What is your name? ");
 
 $first_name = trim(fgets(STDIN));
 
 fwrite(STDOUT, "Hello $first_name\n");
 
-$game_guess = mt_rand(1, 100);
+$game_guess = mt_rand($min, $max);
 
-fwrite(STDOUT, "Guess a number between 1 and 100: ");
+fwrite(STDOUT, "Guess a number between $min and {$max}: ");
 
 $user_guess = fgets(STDIN);
 
