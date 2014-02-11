1ff4d03eeac6874970794cb3c8703b81d0aca305
IndianAg0711 <joseph.villafranca@gmail.com>
2014-02-11 10:58:41 -0600

    functions intro

---
+++ php/arithmetic.php
@@ -0,0 +1,25 @@
+<?php
+
+function add($a, $b) {
+    echo $a + $b . PHP_EOL;
+}
+
+function subtract($a, $b) {
+    echo $a - $b . PHP_EOL;
+}
+
+function multiply($a, $b) {
+	echo $a * $b . PHP_EOL;
+}
+
+function divide($a, $b) {
+	echo $a / $b . PHP_EOL;
+}
+
+
+// add(4,5);
+// subtract(5,2);
+// multiply(9,8);
+// divide(9,3);
+
+?>
