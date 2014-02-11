c0d2ae31ec9003e3d1c371d417af8a97d110fb27
IndianAg0711 <joseph.villafranca@gmail.com>
2014-02-11 13:43:02 -0600

    functions calling functions

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -1,29 +1,57 @@
 <?php
 
+function error($a, $b) {
+	echo "Error. You input $a and $b. Please use numbers." . PHP_EOL;
+}
+
 function add($a, $b) {
+	if (is_numeric($a) && is_numeric($b)) {
     echo $a + $b . PHP_EOL;
+}	else {
+		error($a, $b);
+
+}
 }
 
 function subtract($a, $b) {
+	if (is_numeric($a) && is_numeric($b)) {
     echo $a - $b . PHP_EOL;
+}	else {
+		error($a, $b);
+}
 }
 
 function multiply($a, $b) {
+	if (is_numeric($a) && is_numeric($b)) {
 	echo $a * $b . PHP_EOL;
+}	else {
+		error($a, $b);
+}
 }
 
 function divide($a, $b) {
-	echo $a / $b . PHP_EOL;
+	if ($b == 0) {
+		echo "You cannot divide by 0!" . PHP_EOL;
+	}
+		elseif (is_numeric($a) && is_numeric($b)) {
+			echo $a / $b . PHP_EOL;
+}			else {
+				error($a, $b);
+}
 }
 
 function modulus($a, $b) {
+	if (is_numeric($a) && is_numeric($b)) {
 	echo $a % $b . PHP_EOL;
+}	else {
+		error($a, $b);
+}
 }
 
-add(4,5);
+add('a',5);
 subtract(5,2);
-multiply(9,8);
-divide(9,3);
+multiply(9,'n');
+divide(9,0);
 modulus(9,2);
 
 ?>
