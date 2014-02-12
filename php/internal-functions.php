d4c4addf8d040401d6ae092eac678c6c74728336
IndianAg0711 <joseph.villafranca@gmail.com>
2014-02-12 10:04:02 -0600

    internal functions

---
+++ php/internal-functions.php
@@ -0,0 +1,46 @@
+<?php
+
+$nothing = NULL;
+$something = '';
+$array = array(1,2,3);
+
+// Create a funciton that checks if a variable is set or empty, and display "$variable_name is SET|EMPTY"
+function checkSet($a) {
+	if (isset($a)) {
+		return true;
+	}
+		else {
+			return false;
+		}
+}
+
+if (checkSet($nothing)) {
+	echo "\$nothing is set\n";
+}	else {
+	echo "\$nothing is empty\n";
+}
+
+
+if (checkSet($something)) {
+	echo "\$something is set\n";
+}	else {
+	echo "\$something is empty\n";
+}
+
+
+if (checkSet($nothing)) {
+	echo "\$array is set\n";
+}	else {
+	echo "\$array is empty\n";
+}
+
+
+// TEST: If var $nothing is set, display '$nothing is SET'
+
+// TEST: If var $nothing is empty, display '$nothing is EMPTY'
+
+// TEST: If var $something is set, display '$something is SET'
+
+// Serialize the array $array, and output the results
+
+// Unserialize the array $array, and output the results
