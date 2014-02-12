e564bd0aad351b3b9f21cb6accfed86e651b0976
IndianAg0711 <joseph.villafranca@gmail.com>
2014-02-12 10:15:44 -0600

    empty check

--- php/internal-functions.php
+++ php/internal-functions.php
@@ -6,11 +6,11 @@ $array = array(1,2,3);
 
 // Create a funciton that checks if a variable is set or empty, and display "$variable_name is SET|EMPTY"
 function checkSet($a) {
-	if (isset($a)) {
-		return true;
+	if (empty($a)) {
+		return false;
 	}
 		else {
-			return false;
+			return true;
 		}
 }
 
