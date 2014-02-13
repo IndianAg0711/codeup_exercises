9fbe208c4e052f00f2a4360be75c5747246543e1
IndianAg0711 <joseph.villafranca@gmail.com>
2014-02-13 13:58:11 -0600

    list with commas

---
+++ php/list_with_commas.php
@@ -0,0 +1,6 @@
+<?php
+
+$famous_fake_physicists = "Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Malory, Bruce Banner, and Tony Stark";
+
+echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}.";
+
