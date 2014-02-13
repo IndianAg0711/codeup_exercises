b0a4e493edba45b82ea1f46cf5821afcfcd88256
IndianAg0711 <joseph.villafranca@gmail.com>
2014-02-13 09:55:53 -0600

    show's how many intersections between arrays

--- php/search_arrays.php
+++ php/search_arrays.php
@@ -5,17 +5,23 @@ $names = ['Tina', 'Dana', 'Mike', "Amy", 'Adam'];
 
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
-function find($name, $array) {
-	$result = array_search($name, $array);
-	if (is_int($result)) {
-		echo "True".PHP_EOL;
-		return true;
-	}	else {
-			echo "False".PHP_EOL;
-			return false;
-	}
-}
+// function ($name, $array) {
+// 	$result = array_search($name, $array);
+// 	if (is_int($result)) {
+// 		echo "True".PHP_EOL;
+// 		return true;
+// 	}	else {
+// 			echo "False".PHP_EOL;
+// 			return false;
+// 	}
+// }
 
-find('Tina', $names);
+$array_comp = array_intersect($names, $compare);
 
-find('Bob', $names);
+$comp_count = count($array_comp);
+
+echo $comp_count . " names in common" . PHP_EOL;
+
+// find('Tina', $names);
+
+// find('Bob', $names);
