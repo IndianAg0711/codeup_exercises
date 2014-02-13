bd7451e110ab29113f71876d3f42c4ac19a5e383
IndianAg0711 <joseph.villafranca@gmail.com>
2014-02-13 14:26:13 -0600

    added list function

--- php/list_with_commas.php
+++ php/list_with_commas.php
@@ -1,6 +1,14 @@
 <?php
 
-$famous_fake_physicists = "Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Malory, Bruce Banner, and Tony Stark";
+function humanized_list($string) {
+	$array = explode(', ', $string);
+	$last_item = array_pop($array);
+	$string = implode(', ', $array);
+	$list = $string . ', and ' . $last_item . PHP_EOL;
+	return $list;
+}
+$famous_fake_physicists = "Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Malory, Bruce Banner, Tony Stark";
 
-echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}.";
+$list1 = humanized_list($famous_fake_physicists);
 
+echo $list1;
